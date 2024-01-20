5dd97a47f43a7b98473d433e73d6e15ac124a938
MultiMouse <olmos.luisd@outlook.com>
2024-01-20 09:54:22 -0600

    added functions for checking input on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,48 @@
 export default function Contact() {
+  const checkEmail = () => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailInput = document.querySelector("#email-input").value;
+    const emailAlert = document.querySelector("#email-alert");
+    const emptyEmailAlert = document.querySelector("#empty-email-alert");
+
+    if (emailInput == "") {
+      //checks to see if the input is empty
+      emptyEmailAlert.classList.remove("d-none");
+      emailAlert.classList.add("d-none");
+    } else {
+      if (!emailInput.match(emailRegex)) {
+        //if theres something, check regex format
+        emailAlert.classList.remove("d-none");
+        emptyEmailAlert.classList.add("d-none");
+      } else {
+        emailAlert.classList.add("d-none");
+        emptyEmailAlert.classList.add("d-none");
+      }
+    }
+  };
+
+  const checkName = () => {
+    const nameInput = document.querySelector("#name-input").value;
+    const nameAlert = document.querySelector("#name-alert");
+
+    if (nameInput == "") {
+      nameAlert.classList.remove("d-none");
+    } else {
+      nameAlert.classList.add("d-none");
+    }
+  };
+
+  const checkMessage = () => {
+    const messageInput = document.querySelector("#floatingTextArea").value;
+    const messageAlert = document.querySelector("#message-alert");
+
+    if (messageInput == "") {
+      messageAlert.classList.remove("d-none");
+    } else {
+      messageAlert.classList.add("d-none");
+    }
+  };
+
   return (
     <main className="container">
       <div className="row text-center text-lg-start">
@@ -14,8 +58,16 @@ export default function Contact() {
             className="form-control"
             placeholder="Name..."
             id="name-input"
+            onBlur={() => checkName()}
             required
           />
+          <div
+            id="name-alert"
+            className={`alert alert-danger d-none`}
+            role="alert"
+          >
+            This field needs to be filled out!
+          </div>
         </div>
         <div className="mb-3">
           <label htmlFor="email-input" className="form-label fs-2">
@@ -26,8 +78,23 @@ export default function Contact() {
             className="form-control"
             placeholder="E-mail..."
             id="email-input"
+            onBlur={() => checkEmail()}
             required
           />
+          <div
+            id="empty-email-alert"
+            className={`alert alert-danger d-none`}
+            role="alert"
+          >
+            This field needs to be filled out!
+          </div>
+          <div
+            id="email-alert"
+            className={`alert alert-danger d-none`}
+            role="alert"
+          >
+            Email needs to be in a correct format!
+          </div>
         </div>
         <div className="mb-3">
           <label htmlFor="floatingTextArea" className="fs-2">
@@ -38,8 +105,16 @@ export default function Contact() {
             placeholder="Message..."
             id="floatingTextArea"
             style={{ height: "150px" }}
+            onBlur={() => checkMessage()}
             required
           />
+          <div
+            id="message-alert"
+            className={`alert alert-danger d-none`}
+            role="alert"
+          >
+            This field needs to be filled out!
+          </div>
         </div>
         <div className="mb-5">
           <button type="button" className="btn btn-success fs-4 mb-5">
