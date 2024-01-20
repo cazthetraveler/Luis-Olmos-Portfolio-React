fb118ac60cd2dc92d209368c8a47ab9ec6fe3144
MultiMouse <olmos.luisd@outlook.com>
2024-01-19 22:28:07 -0600

    added projects.js

---
+++ src/projects.js
@@ -0,0 +1,89 @@
+const projects = [
+  {
+    id: 1,
+    title: "Pixel Pets",
+    description:
+      "A simple virtual pet game made with model view controllers and node.js!",
+    links: {
+      repo: "https://github.com/cazthetraveler/Pixel-Pets",
+      deployed: "https://pixel-pets-8842a450b767.herokuapp.com/",
+    },
+    image: {
+      source:
+        "https://github.com/cazthetraveler/Pixel-Pets/blob/main/public/images/pets/pixel_mouse.png?raw=true",
+      alt: "pixel mouse image",
+    },
+  },
+  {
+    id: 2,
+    title: "Coding Quiz",
+    description: "A ten question quiz made with javascript, about javascript!",
+    links: {
+      repo: "https://github.com/cazthetraveler/Coding-Quiz",
+      deployed: "https://cazthetraveler.github.io/Coding-Quiz/",
+    },
+    image: {
+      source:
+        "https://github.com/cazthetraveler/Coding-Quiz/raw/main/assets/images/screenshot.png",
+      alt: "coding quiz image",
+    },
+  },
+  {
+    id: 3,
+    title: "Weather Dashboard",
+    description:
+      "A weather dashboard to display current weather and 5 days of forecast for whatever city you input, made utilizing web API.",
+    links: {
+      repo: "https://github.com/cazthetraveler/Weather-Dashboard",
+      deployed: "https://cazthetraveler.github.io/Weather-Dashboard/",
+    },
+    image: {
+      source:
+        "https://github.com/cazthetraveler/Weather-Dashboard/raw/main/assets/images/screenshot.png",
+      alt: "weather dashboard image",
+    },
+  },
+  {
+    id: 4,
+    title: "weeklyMoodal",
+    description:
+      "A mental health tracker group project that utilizes web API's and local storage.",
+    links: {
+      repo: "https://github.com/jameslwilson364/weeklyMoodal",
+      deployed: "https://jameslwilson364.github.io/weeklyMoodal/",
+    },
+    image: {
+      source:
+        "https://github.com/jameslwilson364/weeklyMoodal/raw/main/assets/images/Live-Screenshot.png",
+      alt: "weekly moodal image",
+    },
+  },
+  {
+    id: 5,
+    title: "",
+    description: "",
+    links: {
+      repo: "",
+      deployed: "",
+    },
+    image: {
+      source: "",
+      alt: "",
+    },
+  },
+  {
+    id: 6,
+    title: "",
+    description: "",
+    links: {
+      repo: "",
+      deployed: "",
+    },
+    image: {
+      source: "",
+      alt: "",
+    },
+  },
+];
+
+export default projects;
